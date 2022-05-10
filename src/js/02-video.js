0f1f377e0adeb6cef8423db8ef62cb84aff7feb8
Marina <sheremetamaryna@gmail.com>
2022-05-10 02:12:26 +0200

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+//on
+const onPlay = function(data) {
+    data.currentTime.Player
+
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(onPlay))
+
+};
+
+player.on('play', onPlay);
+
+//setCurrentTime(seconds: number)
+player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea')
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input',throttle (onTextareaInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state'
+const formData =[];
+//взять value и записать в localstoge
+
+function onTextareaInput (evt) {
+formData[evt.target.name] = evt.target.value;
+
+localStorage.setItem( STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit (evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY)
+
+}
+function populateTextArea() {
+    const savedMessage = JSON.stringify( localStorage.getItem(formData));
+    if (savedMessage) {
+        console.log (savedMessage)
+        refs.textarea.value = savedMessage
+    }
+}
+populateTextArea();
