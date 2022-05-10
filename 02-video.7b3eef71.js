7e8d6651c49e4d147a92c65363e85a7eb6abe67f
emsheremeta <93739364+emsheremeta@users.noreply.github.com>
2022-05-10 13:29:20 +0000

    Deploying to gh-pages from @ emsheremeta/goit-js-hw-08@f11c0d5acb54fdff919cc1e371bf805153358dac 🚀

--- 02-video.7b3eef71.js
+++ 02-video.7b3eef71.js
@@ -4,4 +4,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=t(require("lodash.throttle"));function t(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe"),r=new Vimeo.Player(o);r.on("timeupdate",function(){console.log("played the video!")});const n="videoplayer-current-time";function l(e){localStorage.setItem(n,e.seconds)}r.on("timeupdate",(0,e.default)(l,1e3));const a=localStorage.getItem(n);a&&r.setCurrentTime(localStorage.getItem(n));
 },{"lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/emsheremeta/goit-js-hw-08/02-video.7b3eef71.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.7b3eef71.js.map
