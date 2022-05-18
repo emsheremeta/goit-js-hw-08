03d0309db765dcbb890348956a6b27eb12b98c19
Marina <sheremetamaryna@gmail.com>
2022-05-18 21:42:07 +0200

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,10 +10,13 @@ refs.form.addEventListener('submit', onFormSubmit);
 refs.input.addEventListener('input', throttle(onInput, 500));
 refs.textarea.addEventListener('input', throttle(onInput, 500));
 
+
 const STORAGE_KEY = 'feedback-form-state'
 const formData = {};
-//взять value и записать в localstoge
 
+populateTextArea();
+
+//взять value и записать в localstoge
 function onInput (evt) {
  formData[evt.target.name] = evt.target.value;
  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
@@ -21,7 +24,6 @@ function onInput (evt) {
 
 function onFormSubmit (evt) {
     evt.preventDefault();
-    populateTextArea();
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY)
     
