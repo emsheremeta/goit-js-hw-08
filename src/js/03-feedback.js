c9d635c6340b34688da5f85af1c46a8fbac7016e
Marina <sheremetamaryna@gmail.com>
2022-05-20 10:35:00 +0200

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,24 @@
 import throttle from "lodash.throttle";
 
-const refs = {
+/*const refs = {
     form: document.querySelector('.feedback-form'),
-    email: document.querySelector(".feedback-form input"),
+    email: document.querySelector('.feedback-form input'),
     message: document.querySelector('.feedback-form textarea')
 };
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.email.addEventListener('input', throttle(onInput, 500));
-refs.message.addEventListener('input', throttle(onInput, 500));
+refs.form.addEventListener('submit', throttle(onFormSubmit, 500));
+// refs.email.addEventListener('input', throttle(onInput, 500));
+// refs.message.addEventListener('input', throttle(onInput, 500));
 
 
 const STORAGE_KEY = 'feedback-form-state'
-const formData = {};
+//const formData = {};
 
 populateTextArea();
 
 //взять value и записать в localstoge
 function onInput (evt) {
- formData[evt.target.name] = evt.target.value;
+ //formData[evt.target.name] = evt.target.value;
  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
@@ -49,3 +49,40 @@ function populateTextArea() {
 
 }
 
+*/
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea')
+};
+const STORAGE_KEY = 'feedback-form-state'
+
+populateTextArea ();
+
+refs.form.addEventListener ('submit', evt => {
+    evt.preventDefault();
+    const formData = new FormData (refs.form);
+    formData.forEach((value, name) => console.log (value, name));
+});
+
+refs.form.addEventListener ('change', evt => {
+    let fields = localStorage.getItem(STORAGE_KEY);
+    fields = fields ? JSON.parse(fields) : {}; //проверить если fields равно null, если там что то есть внутри то разпарсить, если нет вернуть пустой обьект
+    fields[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(fields));
+});
+
+refs.form.addEventListener('reset', () => {
+    localStorage.removeItem(STORAGE_KEY);
+});
+
+function populateTextArea () {
+    let fields = localStorage.getItem(STORAGE_KEY);
+    if (fields) {
+        fields = JSON.parse(fields);
+      Object.entries(fields).forEach(([name, value]) => {
+        refs[name].value = value;
+      });
+    }
+} 
